afdd849a763ea005ed75fb6402fde22ef90bd983
hehaiyangwork <hehaiyangwork@qq.com>
2016-01-18 16:30:53 +0800

    add jsp

--- src/main/java/club/itbus/controller/SimpleController.java
+++ src/main/java/club/itbus/controller/SimpleController.java
@@ -1,10 +1,14 @@
 package club.itbus.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,13 +18,15 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * @RestController注解作用：为该controller类下的所有方法添加@ResponseBody注解
  */
-@RestController
+//@RestController
+@Controller
 @RequestMapping
 public class SimpleController {
 
     @Value("${appName}")
     private String appName;
 
+    @ResponseBody
     @RequestMapping(method = RequestMethod.GET)
     public Map<String,Object> index(HttpServletRequest request, HttpServletResponse response) {
         Map<String,Object> map = new HashMap<String, Object>();
@@ -28,5 +34,22 @@ public class SimpleController {
         map.put("appName", appName);
         return map;
     }
+
+    @RequestMapping(value = "/jsp", method = RequestMethod.GET)
+    public ModelAndView jsp(){
+        ModelMap model = new ModelMap();
+        model.addAttribute("app", this.appName);
+        model.addAttribute("time", new Date());
+        return new ModelAndView("welcome", model);
+    }
+
+
+    @RequestMapping(value = "/jsp2", method = RequestMethod.GET)
+    public String jsp2(HttpServletRequest request){
+        request.setAttribute("app", this.appName);
+        request.setAttribute("time", new Date());
+        return "welcome";
+    }
+
 
 }
