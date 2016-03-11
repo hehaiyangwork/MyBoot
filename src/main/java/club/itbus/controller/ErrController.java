70e79a088b863582d768f19697e775e6b35bc5a0
hehaiyangwork <hehaiyangwork@qq.com>
2016-03-11 15:57:36 +0800

    fix one bug

---
+++ src/main/java/club/itbus/controller/ErrController.java
@@ -0,0 +1,26 @@
+package club.itbus.controller;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Desc:
+ * author:HeHaiYang
+ * Date:16/3/11
+ */
+@RestController
+public class ErrController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
