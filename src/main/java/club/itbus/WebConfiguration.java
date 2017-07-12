78a32b47456155fe47377006f62069c0bd3b547a
hehaiyangwork <hehaiyangwork@qq.com>
2017-07-12 10:47:04 +0800

    fix jsp error

---
+++ src/main/java/club/itbus/WebConfiguration.java
@@ -0,0 +1,34 @@
+package club.itbus;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+@EnableAutoConfiguration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer(){
+        return new EmbeddedServletContainerCustomizer(){
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"));
+                container.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500"));
+                container.addErrorPages(new ErrorPage(java.lang.Throwable.class,"/500"));
+            }
+        };
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/pages/500.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hehaiyang
+  Date: 2017/7/12
+  Time: 09:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+timestamp:{$timestamp}
+status:{$status}
+error:{$error}
+exception:{$exception}
+message:{$message}
+path:{$path}
+</body>
+</html>
