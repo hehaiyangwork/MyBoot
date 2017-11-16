02e936b0592d7a13f64229eff2de5ee9b2a98959
hehaiyangwork <hehaiyangwork@qq.com>
2017-11-16 11:16:26 +0800

    add handlebars

--- src/main/java/cn/hykes/boot/WebConfiguration.java
+++ src/main/java/cn/hykes/boot/WebConfiguration.java
@@ -3,6 +3,7 @@ package cn.hykes.boot;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan
 @EnableWebMvc
 @EnableAutoConfiguration
+@Import(HandlebarsAutoConfiguration.class)
 public class WebConfiguration extends WebMvcConfigurerAdapter {
-
 
 }
--- src/main/java/cn/hykes/boot/controller/SimpleController.java
+++ src/main/java/cn/hykes/boot/controller/SimpleController.java
@@ -1,12 +1,15 @@
 package cn.hykes.boot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,4 +29,9 @@ public class SimpleController {
         return map;
     }
 
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView index(ModelMap model){
+        model.addAttribute("time", new Date());
+        return new ModelAndView("index", model);
+    }
 }
